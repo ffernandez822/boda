f4d7a699296a74c28518308340902800717e7525
Facundo Fernandez <facundo.fernandez@externos.bci.cl>
2023-09-25 17:30:34 -0300

    ajustes contador + estilos

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-
 $(document).ready(function(){
+    /* ---- Countdown timer ---- */
 
+    // Calcula la fecha de finalización (por ejemplo, 2023-12-31).
+    var endDate = new Date("2023-11-25T21:00:00");
 
-	/* ---- Countdown timer ---- */
+    // Calcula la diferencia en milisegundos entre la fecha final y la fecha actual.
+    var timeDiffMilliseconds = endDate - new Date();
 
-	$('#counter').countdown({
-		timestamp : (new Date()).getTime() + 08*199*65*60*1000
-	});
-
+    // Inicializa el contador con la diferencia de tiempo calculada.
+    $('#counter').countdown({
+        timestamp: (new Date()).getTime() + timeDiffMilliseconds
+    });
 });
